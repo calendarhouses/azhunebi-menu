479e8fd24c7ff9706a8f1c071c73f475cf46bbb7
DuzhTech <mindecho2222@gmail.com>
2026-06-16 13:00:21 +0300

    «9»

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Це потрібно, щоб картинки і стилі правильно завантажувалися за тимчасовим посиланням GitHub
+  basePath: '/azhunebi-menu', 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
